bebc5fbce89e3ecd39f61dc5ceb7fa0379d2a766
Ansh <132185361+choxxin@users.noreply.github.com>
2025-01-05 01:52:10 +0530

    f

--- app/api/runcheck/route.js
+++ app/api/runcheck/route.js
@@ -7,17 +7,7 @@ export async function POST(req) {
   const headers = {
     "Accept-Language": "en-GB,en;q=0.8",
     "Content-Type": "application/json",
-    Referer: "https://leetcode.com/problems/longest-common-prefix/",
-    //Origin:https://leetcode.com
-    //Sec-CH-UA:"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"
-    //Sec-CH-UA-Mobile:?1
-    //Sec-CH-UA-Platform:"Android"
-    //Sec-Fetch-Dest:empty
-    //Sec-Fetch-Mode:same-origin
-    //Sec-GPC:1
-    //User-Agent:	Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36
-    // "X-CSRFToken":
-    //   "irNACXCSl6VcV44kAiRwH9352JWJbz0ZJdYzDq2xbZTjasQcLHNcshIpKeA6FW7H",
+
     Referer: refer, // Correct usage
     "Sec-CH-UA-Mobile": "?1",
     "X-CSRFToken": xcsrftoken, // Correct usage
